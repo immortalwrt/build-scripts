96884a0f5a8b97c70e5571ce6c278548fa3f6f50
Tianling Shen <cnsztl@immortalwrt.org>
2021-06-29 18:59:39 +0800

    init_build_environment: add Ubuntu 20.04 support
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -21,10 +21,10 @@ function check_system(){
 
 	if grep -qo "Ubuntu 18.04" "/etc/issue"; then
 		ubuntu_release="bionic"
-	#elif grep -qo "Ubuntu 20.04" "/etc/issue"; then
-	#	ubuntu_release="focal"
+	elif grep -qo "Ubuntu 20.04" "/etc/issue"; then
+		ubuntu_release="focal"
 	else
-		echo -e "${error_font}Only Ubuntu 18.04 is supported."
+		echo -e "${error_font}Only Ubuntu 18.04 and 20.24 are supported."
 		exit 1
 	fi
 	[ "$(uname -m)" != "x86_64" ] && { echo -e "${error_font}Only x86_64 is supported." && exit 1; }
@@ -56,23 +56,23 @@ function update_apt_source(){
 	mkdir -p "/etc/apt/sources.list.d"
 	mv "/etc/apt/sources.list" "/etc/apt/sources.list.bak"
 	cat <<-EOF >"/etc/apt/sources.list"
-deb http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release} main restricted universe multiverse
-deb http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release}-security main restricted universe multiverse
-deb http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release}-updates main restricted universe multiverse
-deb http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release}-backports main restricted universe multiverse
-# deb http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release}-proposed main restricted universe multiverse
-deb-src http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release} main restricted universe multiverse
-deb-src http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release}-security main restricted universe multiverse
-deb-src http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release}-updates main restricted universe multiverse
-deb-src http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release}-backports main restricted universe multiverse
-# deb-src http://mirrors.cloud.tencent.com/ubuntu/ ${ubuntu_release}-proposed main restricted universe multiverse
+deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release} main restricted universe multiverse
+deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-security main restricted universe multiverse
+deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-updates main restricted universe multiverse
+deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-backports main restricted universe multiverse
+# deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-proposed main restricted universe multiverse
+deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release} main restricted universe multiverse
+deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-security main restricted universe multiverse
+deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-updates main restricted universe multiverse
+deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-backports main restricted universe multiverse
+# deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-proposed main restricted universe multiverse
 	EOF
 
 	sed -i "s#ppa.launchpad.net#launchpad.proxy.ustclug.org#g" /etc/apt/sources.list.d/*
 
 	cat <<-EOF >"/etc/apt/sources.list.d/nodesource.list"
-deb https://mirrors.cloud.tencent.com/nodesource/deb_12.x ${ubuntu_release} main
-deb-src https://mirrors.cloud.tencent.com/nodesource/deb_12.x ${ubuntu_release} main
+deb https://deb.nodesource.com/node_14.x ${ubuntu_release} main
+deb-src https://deb.nodesource.com/node_14.x ${ubuntu_release} main
 	EOF
 	curl -sL "https://deb.nodesource.com/gpgkey/nodesource.gpg.key" | apt-key add -
 
@@ -101,7 +101,7 @@ function install_compilation_dependencies(){
 	echo -e "Installing compilation dependencies..."
 
 	apt full-upgrade -y
-	[ "${ubuntu_release}" = "focal" ] && extra_packages="python-is-python3" || extra_packages="python python-pip python-ply"
+	[ "${ubuntu_release}" = "focal" ] && extra_packages="python2.7" || extra_packages="python python-pip python-ply"
 	apt install -y build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core p7zip p7zip-full msmtp libssl-dev texinfo libreadline-dev libglib2.0-dev xmlto qemu-utils libelf-dev autoconf automake libtool autopoint ccache curl wget vim nano ${extra_packages} python3 python3-pip python3-ply haveged lrzsz device-tree-compiler scons squashfs-tools antlr3 gperf ecj fastjar re2c intltool xxd help2man pkgconf libgmp3-dev libmpc-dev libmpfr-dev libncurses5-dev libltdl-dev python-docutils cpio bison rsync mkisofs
 
 	apt install -y gcc-8 g++-8 gcc-8-multilib g++-8-multilib
@@ -122,13 +122,13 @@ function install_compilation_dependencies(){
 	apt clean -y
 
 	upx_version="3.95"
-	curl -sL "https://github.com/upx/upx/releases/download/v${upx_version}/upx-${upx_version}-amd64_linux.tar.xz" -o "/root/upx-${upx_version}-amd64_linux.tar.xz"
-	tar -xf "/root/upx-${upx_version}-amd64_linux.tar.xz" -C "/root/"
+	curl -sL "https://github.com/upx/upx/releases/download/v${upx_version}/upx-${upx_version}-amd64_linux.tar.xz" -o "/tmp/upx-${upx_version}-amd64_linux.tar.xz"
+	tar -xf "/tmp/upx-${upx_version}-amd64_linux.tar.xz" -C "/tmp/"
 	rm -f "/usr/bin/upx" "/usr/bin/upx-ucl"
-	mv "/root/upx-${upx_version}-amd64_linux/upx" "/usr/bin/upx-ucl"
+	mv "/tmp/upx-${upx_version}-amd64_linux/upx" "/usr/bin/upx-ucl"
 	chmod 0755 "/usr/bin/upx-ucl"
 	ln -sf "/usr/bin/upx-ucl" "/usr/bin/upx"
-	rm -rf "/root/upx-${upx_version}-amd64_linux.tar.xz" "/root/upx-${upx_version}-amd64_linux"
+	rm -rf "/tmp/upx-${upx_version}-amd64_linux.tar.xz" "/tmp/upx-${upx_version}-amd64_linux"
 
 	curl -sL "https://build-scripts.project-openwrt.eu.org/init_build_environment/modify-firmware" -o "/usr/bin/modify-firmware"
 	chmod 0755 "/usr/bin/modify-firmware"
