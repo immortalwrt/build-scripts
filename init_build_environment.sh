87689b1f9ee4a1a54eb2a8f6ec05f85fbea8a08b
Tianling Shen <cnsztl@immortalwrt.org>
2021-11-12 20:49:35 +0800

    init_build_environment: refactor script
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -1,78 +1,82 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# Copyright (C) ImmortalWrt.org
 
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+DEFAULT_COLOR="\033[0m"
+BLUE_COLOR="\033[36m"
+GREEN_COLOR="\033[32m"
+RED_COLOR="\033[31m"
+YELLOW_COLOR="\033[33m"
 
-function set_fonts_colors(){
-	default_fontcolor="\033[0m"
-	red_fontcolor="\033[31m"
-	green_fontcolor="\033[32m"
-	warning_fontcolor="\033[33m"
-	info_fontcolor="\033[36m"
+function __error_msg() {
+	echo -e "${RED_COLOR}[ERROR]${DEFAULT_COLOR} $*"
+}
 
-	error_font="${red_fontcolor}[Error]${default_fontcolor} "
-	ok_font="${green_fontcolor}[OK]${default_fontcolor} "
-	warning_font="${warning_fontcolor}[Warning]${default_fontcolor} "
-	info_font="${info_fontcolor}[Info]${default_fontcolor} "
+function __info_msg() {
+	echo -e "${BLUE_COLOR}[INFO]${DEFAULT_COLOR} $*"
+}
+
+function __success_msg() {
+	echo -e "${GREEN_COLOR}[SUCCESS]${DEFAULT_COLOR} $*"
+}
+
+function __warning_msg() {
+	echo -e "${YELLOW_COLOR}[WARNING]${DEFAULT_COLOR} $*"
 }
 
 function check_system(){
-	echo -e "${info_font}Checking system info..."
+	__info_msg "Checking system info..."
 
 	if grep -qo "Ubuntu 18.04" "/etc/issue"; then
-		ubuntu_release="bionic"
+		UBUNTU_RELEASE="bionic"
 	elif grep -qo "Ubuntu 20.04" "/etc/issue"; then
-		ubuntu_release="focal"
+		UBUNTU_RELEASE="focal"
 	else
-		echo -e "${error_font}Only Ubuntu 18.04 and 20.04 are supported."
+		__error_msg "Unsupported OS, use Ubuntu 20.04 instead."
 		exit 1
 	fi
-	[ "$(uname -m)" != "x86_64" ] && { echo -e "${error_font}Only x86_64 is supported." && exit 1; }
 
-	echo -e "${ok_font}Done."
-}
+	[ "$(uname -m)" != "x86_64" ] && { __error_msg "Unsupported architecture, use AMD64 instead." && exit 1; }
 
-function check_user(){
-	echo -e "${info_font}Checking user info..."
-
-	[ "$(whoami)" != "root" ] && { echo -e "${error_font}You must run as root." && exit 1; }
-
-	echo -e "${ok_font}Done."
+	[ "$(whoami)" != "root" ] && { __error_msg "You must run me as root." && exit 1; }
 }
 
 function check_network(){
-	echo -e "${info_font}Checking network..."
+	__info_msg "Checking network..."
 
-	curl -s "myip.ipip.net" | grep -qo "中国" && country_cn="Y"
-	curl --connect-timeout 10 "baidu.com" > "/dev/null" 2>&1 || { echo -e "${warning_font}Your network is not suitable for compiling OpenWrt!"; }
-	curl --connect-timeout 10 "google.com" > "/dev/null" 2>&1 || { echo -e "${warning_font}Your network is not suitable for compiling OpenWrt!"; }
-
-	echo -e "${ok_font}Done."
+	curl -s "myip.ipip.net" | grep -qo "中国" && CHN_NET=1
+	curl --connect-timeout 10 "baidu.com" > "/dev/null" 2>&1 || { __warning_msg "Your network is not suitable for compiling OpenWrt!"; }
+	curl --connect-timeout 10 "google.com" > "/dev/null" 2>&1 || { __warning_msg "Your network is not suitable for compiling OpenWrt!"; }
 }
 
 function update_apt_source(){
-	echo -e "${info_font}Updating apt source lists..."
+	__info_msg "Updating apt source lists..."
+	set -x
 
-	mkdir -p "/etc/apt/sources.list.d"
-	[ -n "${country_cn}" ] && {
+	apt update -y
+	apt install -y apt-transport-https
+	[ -n "$CHN_NET" ] && {
 		mv "/etc/apt/sources.list" "/etc/apt/sources.list.bak"
 		cat <<-EOF >"/etc/apt/sources.list"
-			deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release} main restricted universe multiverse
-			deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-security main restricted universe multiverse
-			deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-updates main restricted universe multiverse
-			deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-backports main restricted universe multiverse
-			# deb http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-proposed main restricted universe multiverse
-			deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release} main restricted universe multiverse
-			deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-security main restricted universe multiverse
-			deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-updates main restricted universe multiverse
-			deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-backports main restricted universe multiverse
-			# deb-src http://mirrors.tencent.com/ubuntu/ ${ubuntu_release}-proposed main restricted universe multiverse
+			deb http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE main restricted universe multiverse
+			deb http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE-security main restricted universe multiverse
+			deb http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE-updates main restricted universe multiverse
+			# deb http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE-proposed main restricted universe multiverse
+			deb http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE-backports main restricted universe multiverse
+			deb-src http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE main restricted universe multiverse
+			deb-src http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE-security main restricted universe multiverse
+			deb-src http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE-updates main restricted universe multiverse
+			deb-src http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE-backports main restricted universe multiverse
+			# deb-src http://mirrors.tencent.com/ubuntu/ $UBUNTU_RELEASE-proposed main restricted universe multiverse
 		EOF
 	}
 
+	mkdir -p "/etc/apt/sources.list.d"
+
 	cat <<-EOF >"/etc/apt/sources.list.d/nodesource.list"
-		deb https://deb.nodesource.com/node_16.x ${ubuntu_release} main
-		deb-src https://deb.nodesource.com/node_16.x ${ubuntu_release} main
+		deb https://deb.nodesource.com/node_16.x $UBUNTU_RELEASE main
+		deb-src https://deb.nodesource.com/node_16.x $UBUNTU_RELEASE main
 	EOF
 	curl -sL "https://deb.nodesource.com/gpgkey/nodesource.gpg.key" | apt-key add -
 
@@ -82,29 +86,30 @@ function update_apt_source(){
 	curl -sL "https://dl.yarnpkg.com/debian/pubkey.gpg" | apt-key add -
 
 	cat <<-EOF >"/etc/apt/sources.list.d/gcc-toolchain.list"
-		deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu ${ubuntu_release} main
-		deb-src http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu ${ubuntu_release} main
+		deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu $UBUNTU_RELEASE main
+		deb-src http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu $UBUNTU_RELEASE main
 	EOF
 	curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x1e9377a2ba9ef27f" | apt-key add -
 
-	cat <<-EOF >"/etc/apt/sources.list.d/longsleep-ubuntu-golang-backports-${ubuntu_release}.list"
-		deb http://ppa.launchpad.net/longsleep/golang-backports/ubuntu ${ubuntu_release} main
-		deb-src http://ppa.launchpad.net/longsleep/golang-backports/ubuntu ${ubuntu_release} main
+	cat <<-EOF >"/etc/apt/sources.list.d/longsleep-ubuntu-golang-backports-$UBUNTU_RELEASE.list"
+		deb http://ppa.launchpad.net/longsleep/golang-backports/ubuntu $UBUNTU_RELEASE main
+		deb-src http://ppa.launchpad.net/longsleep/golang-backports/ubuntu $UBUNTU_RELEASE main
 	EOF
 	curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x52b59b1571a79dbc054901c0f6bc817356a3d45e" | apt-key add -
 
-	[ -n "${country_cn}" ] && sed -i "s#http://ppa.launchpad.net#https://launchpad.proxy.ustclug.org#g" "/etc/apt/sources.list.d"/*
+	[ -n "$CHN_NET" ] && sed -i "s,http://ppa.launchpad.net,https://launchpad.proxy.ustclug.org,g" "/etc/apt/sources.list.d"/*
 
 	apt update -y
 
-	echo -e "${ok_font}Done."
+	set +x
 }
-function install_compilation_dependencies(){
-	echo -e "Installing compilation dependencies..."
+function install_dependencies(){
+	__info_msg "Installing dependencies..."
+	set -x
 
 	apt full-upgrade -y
-	[ "${ubuntu_release}" = "focal" ] && extra_packages="python2.7" || extra_packages="python python-pip python-ply"
-	apt install -y ${extra_packages} ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+	[ "$UBUNTU_RELEASE" = "focal" ] && EXTRA_PKG="python2.7" || EXTRA_PKG="python python-pip python-ply"
+	apt install -y $EXTRA_PKG ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
 		bzip2 ccache cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext git git-core gperf haveged \
 		help2man intltool lib32gcc1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev \
 		libmpfr-dev libncurses5-dev libncurses5-dev libreadline-dev libssl-dev libtool libz-dev lrzsz mkisofs msmtp \
@@ -112,58 +117,74 @@ function install_compilation_dependencies(){
 		re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs unzip vim wget xmlto xxd zlib1g-dev
 
 	apt install -y gcc-8 g++-8 gcc-8-multilib g++-8-multilib
-	ln -sf "/usr/bin/gcc-8" "/usr/bin/gcc"
-	ln -sf "/usr/bin/g++-8" "/usr/bin/g++"
-	ln -sf "/usr/bin/gcc-ar-8" "/usr/bin/gcc-ar"
-	ln -sf "/usr/bin/gcc-nm-8" "/usr/bin/gcc-nm"
-	ln -sf "/usr/bin/gcc-ranlib-8" "/usr/bin/gcc-ranlib"
-	ln -sf "/usr/include/asm-generic" "/usr/include/asm"
+	ln -svf "/usr/bin/gcc-8" "/usr/bin/gcc"
+	ln -svf "/usr/bin/g++-8" "/usr/bin/g++"
+	ln -svf "/usr/bin/gcc-ar-8" "/usr/bin/gcc-ar"
+	ln -svf "/usr/bin/gcc-nm-8" "/usr/bin/gcc-nm"
+	ln -svf "/usr/bin/gcc-ranlib-8" "/usr/bin/gcc-ranlib"
+	ln -svf "/usr/include/asm-generic" "/usr/include/asm"
 
-	apt install clang-12
-	ln -sf "/usr/bin/clang-12" "/usr/bin/clang"
-	ln -sf "/usr/bin/clang++-12" "/usr/bin/clang++"
-	ln -sf "/usr/bin/clang-cpp-12" "/usr/bin/clang-cpp"
+	apt install -y clang-12
+	ln -svf "/usr/bin/clang-12" "/usr/bin/clang"
+	ln -svf "/usr/bin/clang++-12" "/usr/bin/clang++"
+	ln -svf "/usr/bin/clang-cpp-12" "/usr/bin/clang-cpp"
 
 	apt install -y nodejs yarn
-	[ -n "${country_cn}" ] && {
+	[ -n "$CHN_NET" ] && {
 		npm config set registry "https://registry.npm.taobao.org/" --global
 		yarn config set registry "https://registry.npm.taobao.org/" --global
 	}
 
 	apt install -y golang-1.17-go
-	ln -sf "/usr/lib/go-1.17/bin/go" "/usr/bin/go"
-	ln -sf "/usr/lib/go-1.17/bin/gofmt" "/usr/bin/gofmt"
+	rm -rf "/usr/bin/go" "/usr/bin/gofmt"
+	ln -svf "/usr/lib/go-1.17/bin/go" "/usr/bin/go"
+	ln -svf "/usr/lib/go-1.17/bin/gofmt" "/usr/bin/gofmt"
 
 	apt clean -y
 
-	upx_version="3.95"
-	curl -sL "https://github.com/upx/upx/releases/download/v${upx_version}/upx-${upx_version}-amd64_linux.tar.xz" -o "/tmp/upx-${upx_version}-amd64_linux.tar.xz"
-	tar -xf "/tmp/upx-${upx_version}-amd64_linux.tar.xz" -C "/tmp/"
-	rm -f "/usr/bin/upx" "/usr/bin/upx-ucl"
-	mv "/tmp/upx-${upx_version}-amd64_linux/upx" "/usr/bin/upx-ucl"
+	if TMP_DIR="$(mktemp -d)"; then
+		pushd "$TMP_DIR"
+	else
+		__error_msg "Failed to create a tmp directory."
+		exit 1
+	fi
+
+	UPX_REV="3.95"
+	curl -fLO "https://github.com/upx/upx/releases/download/v${UPX_REV}/upx-$UPX_REV-amd64_linux.tar.xz"
+	tar -Jxf "upx-$UPX_REV-amd64_linux.tar.xz"
+	rm -rf "/usr/bin/upx" "/usr/bin/upx-ucl"
+	cp -fp "upx-$UPX_REV-amd64_linux/upx" "/usr/bin/upx-ucl"
 	chmod 0755 "/usr/bin/upx-ucl"
-	ln -sf "/usr/bin/upx-ucl" "/usr/bin/upx"
-	rm -rf "/tmp/upx-${upx_version}-amd64_linux.tar.xz" "/tmp/upx-${upx_version}-amd64_linux"
+	ln -svf "/usr/bin/upx-ucl" "/usr/bin/upx"
 
-	curl -sL "https://build-scripts.immortalwrt.eu.org/init_build_environment/modify-firmware" -o "/usr/bin/modify-firmware"
+	svn co -r96154 "https://github.com/openwrt/openwrt/trunk/tools/padjffs2/src" "padjffs2"
+	pushd "padjffs2"
+	make
+	rm -rf "/usr/bin/padjffs2"
+	cp -fp "padjffs2" "/usr/bin/padjffs2"
+	popd
+
+	svn co -r19250 "https://github.com/openwrt/luci/trunk/modules/luci-base/src" "po2lmo"
+	pushd "po2lmo"
+	make po2lmo
+	rm -rf "/usr/bin/po2lmo"
+	cp -fp "po2lmo" "/usr/bin/po2lmo"
+	popd
+
+	curl -fL "https://build-scripts.immortalwrt.eu.org/modify-firmware.sh" -o "/usr/bin/modify-firmware"
 	chmod 0755 "/usr/bin/modify-firmware"
-	curl -sL "https://build-scripts.immortalwrt.eu.org/init_build_environment/po2lmo" -o "/usr/bin/po2lmo"
-	chmod 0755 "/usr/bin/po2lmo"
-	curl -sL "https://build-scripts.immortalwrt.eu.org/init_build_environment/padjffs2" -o "/usr/bin/padjffs2"
-	chmod 0755 "/usr/bin/padjffs2"
-	curl -sL "https://build-scripts.immortalwrt.eu.org/init_build_environment/fip_create" -o "/usr/bin/fip_create"
-	chmod 0755 "/usr/bin/fip_create"
 
-	echo -e "Done."
+	popd
+	rm -rf "$TMP_DIR"
+
+	set +x
+	__success_msg "All dependencies have been installed."
 }
 function main(){
-	set_fonts_colors
 	check_system
-	check_user
 	check_network
 	update_apt_source
-	install_compilation_dependencies
-	echo -e "${ok_font}The dependencies of compiling OpenWrt are all installed, thanks for using."
+	install_dependencies
 }
 
 main
---
+++ modify-firmware.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+sudo echo > "/dev/null"
+
+MKSQSHFS4='mksquashfs'
+PADJFFS2='padjffs2'
+UNSQSHFS='unsquashfs'
+
+case "$1" in
+'extract'|'e')
+	offset1="$(grep -oba hsqs "$2" | grep -oP '[0-9]*(?=:hsqs)')"
+	offset2="$(wc -c "$2" | grep -oP '[0-9]*(?= )')"
+	size2="$(("$offset2" - "$offset1"))"
+	#echo $offset1 " " $offset2 " " $size2
+	dd if="$2" of="kernel.bin" bs=1 ibs=1 count="$offset1"
+	dd if="$2" of="secondchunk.bin" bs=1 ibs=1 count="$size2" skip="$offset1"
+	sudo rm -rf "squashfs-root" 2>&1
+	sudo $UNSQSHFS -d "squashfs-root" "secondchunk.bin"
+	rm "secondchunk.bin"
+	;;
+'create'|'c')
+	sudo $MKSQSHFS4 "./squashfs-root" "./newsecondchunk.bin"
+	sudo chown "$USER" "./newsecondchunk.bin"
+	cat "kernel.bin" "newsecondchunk.bin" > "$2"
+	$PADJFFS2 "$2"
+	rm "newsecondchunk.bin"
+	;;
+*)
+	echo "Run \"$0 extract firmware.bin\" you will find file \"kernel.bin\" and folder \"squashfs-root\". Modify \"squashfs-root\" as you like.
+After everything is done, run \"$0 create newfirmware.bin\" and you will get a modified firmware named \"newfirmware.bin\"."
+	;;
+esac
