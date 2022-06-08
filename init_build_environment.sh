eab0f32b20d5635df9d0f2e4b39464cdbd0c0ca5
Tianling Shen <cnsztl@immortalwrt.org>
2022-06-08 19:02:05 +0800

    init_build_environment: install libpython3-dev for u-boot
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -119,9 +119,9 @@ function install_dependencies(){
 		bzip2 ccache cmake cpio curl device-tree-compiler ecj fakeroot fastjar flex gawk gettext git gperf \
 		haveged help2man intltool jq lib32gcc1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
 		libmpc-dev libmpfr-dev libncurses5-dev libncursesw5 libncursesw5-dev libreadline-dev libssl-dev libtool \
-		libyaml-dev libz-dev lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip \
-		python3-ply python-docutils qemu-utils quilt re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs \
-		unzip vim wget xmlto xxd zlib1g-dev
+		libyaml-dev libz-dev lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf libpython3-dev \
+		python3 python3-pip python3-ply python3-docutils qemu-utils quilt re2c rsync scons squashfs-tools \
+		subversion swig texinfo uglifyjs unzip vim wget xmlto xxd zlib1g-dev
 
 	apt install -y gcc-8 g++-8 gcc-8-multilib g++-8-multilib
 	ln -svf "/usr/bin/gcc-8" "/usr/bin/gcc"
