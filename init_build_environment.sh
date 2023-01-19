7b7269f7f9e446d9c2e1432bb1a56309bea69feb
Tianling Shen <cnsztl@immortalwrt.org>
2023-01-20 00:35:29 +0800

    init_build_environment: install gnutls-dev for u-boot
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -118,11 +118,12 @@ function install_dependencies(){
 	apt full-upgrade -y
 	apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
 		bzip2 ccache cmake cpio curl device-tree-compiler ecj fakeroot fastjar flex gawk gettext git \
-		gperf haveged help2man intltool jq libc6-dev-i386 libelf-dev lib32gcc-s1 libglib2.0-dev libgmp3-dev \
-		libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5 libncursesw5-dev libreadline-dev \
-		libssl-dev libtool libyaml-dev libz-dev lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch \
-		pkgconf python2 libpython3-dev python3 python3-pip python3-ply python3-docutils qemu-utils quilt \
-		re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs unzip vim wget xmlto xxd zlib1g-dev
+		gnutls-dev gperf haveged help2man intltool jq libc6-dev-i386 libelf-dev lib32gcc-s1 libglib2.0-dev \
+		libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5 libncursesw5-dev \
+		libreadline-dev libssl-dev libtool libyaml-dev libz-dev lrzsz mkisofs msmtp nano ninja-build \
+		p7zip p7zip-full patch pkgconf python2 libpython3-dev python3 python3-pip python3-ply python3-docutils \
+		qemu-utils quilt re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs unzip vim wget \
+		xmlto xxd zlib1g-dev
 
 	apt install -y gcc-11 g++-11 gcc-11-multilib g++-11-multilib
 	ln -svf "/usr/bin/gcc-11" "/usr/bin/gcc"
