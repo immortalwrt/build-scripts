f0a3cf65eac510b265710137cd6333b1c52c41c1
Tianling Shen <cnsztl@immortalwrt.org>
2023-01-26 23:02:02 +0800

    init_build_environment: install python3-pyelftools for u-boot
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -122,8 +122,8 @@ function install_dependencies(){
 		libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5 libncursesw5-dev \
 		libreadline-dev libssl-dev libtool libyaml-dev libz-dev lrzsz mkisofs msmtp nano ninja-build \
 		p7zip p7zip-full patch pkgconf python2 libpython3-dev python3 python3-pip python3-ply python3-docutils \
-		qemu-utils quilt re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs unzip vim wget \
-		xmlto xxd zlib1g-dev
+		python3-pyelftools qemu-utils quilt re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs \
+		unzip vim wget xmlto xxd zlib1g-dev
 
 	apt install -y gcc-11 g++-11 gcc-11-multilib g++-11-multilib
 	ln -svf "/usr/bin/gcc-11" "/usr/bin/gcc"
