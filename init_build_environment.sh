844defb70905f3560a9e16748da26c12d97924e7
Tianling Shen <cnsztl@immortalwrt.org>
2024-01-11 11:18:08 +0800

    init_build_environment: fix setup padjffs2 and po2lmo
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -225,16 +225,26 @@ function install_dependencies(){
 	chmod 0755 "/usr/bin/upx-ucl"
 	ln -svf "/usr/bin/upx-ucl" "/usr/bin/upx"
 
-	svn co -r161078 "https://github.com/openwrt/openwrt/trunk/tools/padjffs2/src" "padjffs2"
+	git clone --filter=blob:none --no-checkout "https://github.com/openwrt/openwrt.git" "padjffs2"
 	pushd "padjffs2"
+	git config core.sparseCheckout true
+	echo "tools/padjffs2/src" >> ".git/info/sparse-checkout"
+	git checkout
+	cd "tools/padjffs2/src"
 	make
+	strip "padjffs2"
 	rm -rf "/usr/bin/padjffs2"
 	cp -fp "padjffs2" "/usr/bin/padjffs2"
 	popd
 
-	svn co -r19250 "https://github.com/openwrt/luci/trunk/modules/luci-base/src" "po2lmo"
+	git clone --filter=blob:none --no-checkout "https://github.com/openwrt/luci.git" "po2lmo"
 	pushd "po2lmo"
+	git config core.sparseCheckout true
+	echo "modules/luci-base/src" >> ".git/info/sparse-checkout"
+	git checkout
+	cd "modules/luci-base/src"
 	make po2lmo
+	strip "po2lmo"
 	rm -rf "/usr/bin/po2lmo"
 	cp -fp "po2lmo" "/usr/bin/po2lmo"
 	popd
