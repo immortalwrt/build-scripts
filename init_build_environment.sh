4949e8dfd90f610d782408c0205d9a266d868776
Tianling Shen <cnsztl@immortalwrt.org>
2024-10-12 21:08:09 +0800

    init_build_environment: fix buster support
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -183,15 +183,21 @@ function install_dependencies() {
 	set -x
 
 	apt full-upgrade -y $BPO_FLAG
-	apt install -y $BPO_FLAG ack antlr3 asciidoc autoconf automake autopoint \
-		binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler ecj \
-		fakeroot fastjar flex gawk gettext genisoimage git gnutls-dev gperf haveged help2man \
-		intltool jq libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev \
-		libmpfr-dev libncurses-dev libreadline-dev libssl-dev libtool libyaml-dev libz-dev lrzsz \
-		msmtp nano ninja-build p7zip p7zip-full patch pkgconf libpython3-dev python3 python3-pip \
-		python3-ply python3-docutils python3-pyelftools qemu-utils quilt re2c rsync scons \
-		squashfs-tools subversion swig texinfo uglifyjs unzip vim wget xmlto zlib1g-dev zstd \
-		xxd $VERSION_PACKAGE
+	apt install -y $BPO_FLAG ack antlr3 asciidoc autoconf automake autopoint binutils bison \
+		build-essential bzip2 ccache cmake cpio curl device-tree-compiler ecj fakeroot \
+		fastjar flex gawk gettext genisoimage git gnutls-dev gperf haveged help2man \
+		intltool jq libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
+		libmpc-dev libmpfr-dev libncurses-dev libreadline-dev libssl-dev libtool \
+		libyaml-dev libz-dev lrzsz msmtp nano ninja-build p7zip p7zip-full patch \
+		pkgconf libpython3-dev python3 python3-pip python3-ply python3-docutils \
+		python3-pyelftools qemu-utils quilt re2c rsync scons squashfs-tools subversion \
+		swig texinfo uglifyjs unzip vim wget xmlto zlib1g-dev zstd xxd $VERSION_PACKAGE
+
+	# fix broken http2 support for curl on buster
+	if [ "$VERSION_CODENAME" == "buster"]; then
+		apt full-upgrade -y
+		apt reinstall -y libcurl3-gnutls/buster
+	fi
 
 	if [ -n "$CHN_NET" ]; then
 		pip3 config set global.index-url "https://mirrors.aliyun.com/pypi/simple/"
