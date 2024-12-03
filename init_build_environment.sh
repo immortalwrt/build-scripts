97de1e3f35cff8788c5d16502e78807cae6cd42f
Tianling Shen <cnsztl@immortalwrt.org>
2024-12-03 14:22:44 +0800

    init_build_environment: install python3-cryptography for stm32 target
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -189,9 +189,10 @@ function install_dependencies() {
 		intltool jq libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
 		libmpc-dev libmpfr-dev libncurses-dev libreadline-dev libssl-dev libtool \
 		libyaml-dev libz-dev lrzsz msmtp nano ninja-build p7zip p7zip-full patch \
-		pkgconf libpython3-dev python3 python3-pip python3-ply python3-docutils \
-		python3-pyelftools qemu-utils quilt re2c rsync scons sharutils squashfs-tools \
-		subversion swig texinfo uglifyjs unzip vim wget xmlto zlib1g-dev zstd xxd $VERSION_PACKAGE
+		pkgconf libpython3-dev python3 python3-pip python3-cryptography python3-docutils \
+		python3-ply python3-pyelftools qemu-utils quilt re2c rsync scons sharutils \
+		squashfs-tools subversion swig texinfo uglifyjs unzip vim wget xmlto zlib1g-dev \
+		zstd xxd $VERSION_PACKAGE
 
 	# fix broken http2 support for curl on buster
 	if [ "$VERSION_CODENAME" == "buster"]; then
