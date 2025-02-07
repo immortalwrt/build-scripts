62455da689023d0bcba1b3bfc27fddc47c4488b6
Tianling Shen <cnsztl@immortalwrt.org>
2025-02-07 17:14:04 +0800

    init_build_environment: install irqbalance
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -186,9 +186,9 @@ function install_dependencies() {
 	apt install -y $BPO_FLAG ack antlr3 asciidoc autoconf automake autopoint binutils bison \
 		build-essential bzip2 ccache cmake cpio curl device-tree-compiler ecj fakeroot \
 		fastjar flex gawk gettext genisoimage git gnutls-dev gperf haveged help2man \
-		intltool jq libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev \
-		libmpc-dev libmpfr-dev libncurses-dev libreadline-dev libssl-dev libtool \
-		libyaml-dev libz-dev lrzsz msmtp nano ninja-build p7zip p7zip-full patch \
+		intltool irqbalance jq libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev \
+		libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libreadline-dev libssl-dev \
+		libtool libyaml-dev libz-dev lrzsz msmtp nano ninja-build p7zip p7zip-full patch \
 		pkgconf libpython3-dev python3 python3-pip python3-cryptography python3-docutils \
 		python3-ply python3-pyelftools qemu-utils quilt re2c rsync scons sharutils \
 		squashfs-tools subversion swig texinfo uglifyjs unzip vim wget xmlto zlib1g-dev \
