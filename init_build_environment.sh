ca7daf292907912ea7f379081dc400ecedd15e47
Tianling Shen <cnsztl@immortalwrt.org>
2025-06-20 14:46:21 +0800

    init_build_environment: bump golang to 1.24
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- init_build_environment.sh
+++ init_build_environment.sh
@@ -235,10 +235,10 @@ function install_dependencies() {
 		yarn config set registry "https://registry.npmmirror.com" --global
 	fi
 
-	apt install -y $BPO_FLAG golang-1.23-go
+	apt install -y $BPO_FLAG golang-1.24-go
 	rm -rf "/usr/bin/go" "/usr/bin/gofmt"
-	ln -svf "/usr/lib/go-1.23/bin/go" "/usr/bin/go"
-	ln -svf "/usr/lib/go-1.23/bin/gofmt" "/usr/bin/gofmt"
+	ln -svf "/usr/lib/go-1.24/bin/go" "/usr/bin/go"
+	ln -svf "/usr/lib/go-1.24/bin/gofmt" "/usr/bin/gofmt"
 	if [ -n "$CHN_NET" ]; then
 		go env -w GOPROXY=https://goproxy.cn,direct
 	fi
